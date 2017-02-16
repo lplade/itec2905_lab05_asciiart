d88f4edd9a1ee4d820c863e2f6ff44a496c3fbe3
Larry Lade <lplade@users.noreply.github.com>
2017-02-16 13:37:36 -0600

    Online picture fetcher

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 
 setup(
     name="AsciiArt",
-    version="0.2",
+    version="0.3",
     description="ASCII art from images",
     author="lplade",
     packages=["asciiart"],
-    install_requires=["pillow"]
+    install_requires=[
+        "pillow",
+        "requests"
+    ]
 )
